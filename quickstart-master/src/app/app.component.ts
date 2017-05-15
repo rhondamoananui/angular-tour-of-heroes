38fcbc5b16be747eacaf57d6f5f0f10443c9a47c
Rhonda <rhonda808808@gmail.com>
2017-05-15 23:33:00 +1200

    created @Component
    edited the @component template

--- quickstart-master/src/app/app.component.ts
+++ quickstart-master/src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero}}</h2>
+    `
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  myHero = 'Windstorm';
+}
